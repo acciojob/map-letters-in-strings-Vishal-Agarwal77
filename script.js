01d484e59c5f3126d8d66c2463cb1f3a98ba3a43
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-20 02:23:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(str) {
+	let ans={};
+	for(let i=0;i<str.length;i++){
+		if(ans[str.charAt(i)]===undefined){
+			ans[str.charAt(i)]=[i];
+		}
+		else{
+			ans[str.charAt(i)].push(i);
+		}
+	}
+	return ans;
+}
+//
+let str=prompt("Enter text:");
+alert(JSON.stringify(mapLetters(str)));
